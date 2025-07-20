e3078b6dd42e1346478b511e07d37c53c5a5a042
vishalchatarkar <vishalpchatarkar7@gmail.com>
2025-07-20 23:46:55 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,11 @@
-class Test
-{
-    public static void main(String[] args)
-    {
-        System.out.println("My First Java Program.");
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+public class Test {
+
+    private static final Logger logger = Logger.getLogger(Test.class.getName());
+
+    public static void main(String[] args) {
+        logger.info("My First Java Program."); // Compliant: uses logger
     }
 }
